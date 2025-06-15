783222b343fadcbea89d85554b76e4bcf98f4296
CantFindName <39160485+MohdFawaz@users.noreply.github.com>
2025-06-15 21:44:04 +0300

    Adding our Build file

---
+++ build/CMakeFiles/wordcount.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordcount.dir/src/main.cpp.o"
+  "CMakeFiles/wordcount.dir/src/main.cpp.o.d"
+  "wordcount"
+  "wordcount.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordcount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
